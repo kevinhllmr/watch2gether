8d45584a5449444771f23e7646d5490f957d37e8
Kevin Hollmeier <kevin.hollmeier@freenet.de>
2023-07-08 21:19:57 +0200

    redirecting to root

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Home from './components/pages/Home';
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <Router>
+      <Navigate from="*" to="/" />
         <Navbar />
         <Routes>
           <Route path='/' exact Component={Home} />
